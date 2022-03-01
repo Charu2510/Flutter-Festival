4242c8355fc11d8542bdefd9fe50e18217b1f686
Charu Srivastava <charusrivastava2510@gamil.com>
2022-03-01 18:23:12 +0530

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,39 +10,44 @@ class FoodApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text("Hi! Are you hungry?"),
-        ),
-        body: ListView(
-          children :[ 
-            Row(
-              children: [
-                Image.asset("burger.jpg",height: 150,width: 150),
-                const SizedBox(width: 20),
-                const Text("Hey, What about a Burger?"),
-              ],
-            ),
-            Row(
-              children: [
-                Image.asset("pizza.jpg",height: 150,width: 150),
-                const SizedBox(width: 20),
-                const Text("Oh !! How does this pizza look?"),
-              ],
-            ),
-            Row(
-              children: [
-                Image.asset("coca-cola-soda-ice.jpg",height: 150,width: 150),
-                const SizedBox(width: 20),
-                const Text("Wanna have them this Coke?"),
-              ],
-            ),
-          ],
-        ),
-      ), 
+    return Center(
+      child: Container(
+        alignment: Alignment.topCenter,
+        color: Colors.deepPurple, width: 200  ),
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title: const Text("Hi! Are you hungry?"),
+          ),
+          body: ListView(
+            children :[ 
+              Row(
+                children: [
+                  Image.asset("burger.jpg",height: 150,width: 150),
+                  const SizedBox(width: 20),
+                  const Text("Hey, What about a Burger?", style: TextStyle(color: Colors.purple, fontSize: 20)),
+                ],
+              ),
+              Row(
+                children: [
+                  Image.asset("pizza.jpg",height: 150,width: 150),
+                  const SizedBox(width: 20),
+                  const Text("Oh !! How does this pizza look?", style: TextStyle(color: Colors.orange, fontSize: 20)),
+                ],
+              ),
+              Row(
+                children: [
+                  Image.asset("coca-cola-soda-ice.jpg",height: 150,width: 150),
+                  const SizedBox(width: 20),
+                  const Text("Wanna have them with Coke?", style: TextStyle(color: Colors.red, fontSize: 20)),
+                ],
+              ),
+            ],
+          ),
+        ), 
 
+      ),
     );
   }
 }
